fd9fb01c150c025b304681716b9e3e7388ef5bee
Restyled.io <commits@restyled.io>
2020-08-20 00:28:45 +0000

    Restyled by clang-format

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,13 @@
 /*
 COUNTER Instructions
 
-Watch this short video carefully, paying attention to the UI and Chrome Devtools:
+Watch this short video carefully, paying attention to the UI and Chrome
+Devtools:
 https://tk-assets.lambdaschool.com/59036a85-0980-42c8-81ad-9afc8354497f_counter-clip.gif
 
-How many slices of state do you think are necessary to act as "sources of truth" for all
-the things that change in this widget? Give it some thought before continuing reading!
+How many slices of state do you think are necessary to act as "sources of truth"
+for all the things that change in this widget? Give it some thought before
+continuing reading!
 
 A naive developer might say 3 different slices:
   - The count
@@ -16,7 +18,8 @@ But a single slice of state is all that's needed here: the count!
 The other things can simply be _derived_ from the count itself.
 
 STEP 0:
-  Start by studying the component below, and importing the state hook. *********(DONE)
+  Start by studying the component below, and importing the state hook.
+*********(DONE)
 
 STEP 1:
   Using the state hook, create a 'count', 'setCount' pair.
@@ -24,51 +27,56 @@ STEP 1:
 
 STEP 2:
   The 'style' object has the 'color' property hard-coded to "royalblue".
-  What the value of 'color' should be instead is a ternary expression that goes like this:
-  If count is even, then "royalblue", else "crimson".  *********(DONE)
+  What the value of 'color' should be instead is a ternary expression that goes
+like this: If count is even, then "royalblue", else "crimson".  *********(DONE)
 
 STEP 3:
-  We need to replace some hard-coded info in the JSX with expressions, interpolated inside curly brackets.
-  Start by replacing the character "0" with {count}. The 'count' slice of state is the source of truth here.
-  Then, replace the word "even" with a ternary: {if count is even number, then string "even", else string "odd"}.  *********(DONE)
+  We need to replace some hard-coded info in the JSX with expressions,
+interpolated inside curly brackets. Start by replacing the character "0"
+with {count}. The 'count' slice of state is the source of truth here. Then,
+replace the word "even" with a ternary: {if count is even number, then string
+"even", else string "odd"}.  *********(DONE)
 
 STEP 4:
-  This click handler needs to use 'setCount' to schedule the 'count' to become the current 'count' plus one.
-  These state changes are not synchronous: the updated count arrives on the next run of the Counter component.
-  Do NOT simply do count++. The plus plus is forbidden! We never mutate a slice of state in place. Even if you could
-  reassign a const, React would not be aware anything changed. Always use the state updater, passing in a new value.  *********(DONE)
+  This click handler needs to use 'setCount' to schedule the 'count' to become
+the current 'count' plus one. These state changes are not synchronous: the
+updated count arrives on the next run of the Counter component. Do NOT simply do
+count++. The plus plus is forbidden! We never mutate a slice of state in place.
+Even if you could reassign a const, React would not be aware anything changed.
+Always use the state updater, passing in a new value.  *********(DONE)
 
 STEP 5:
-  This click handler needs to use 'setCount' to set the 'count' to be the current 'count' minus one.
-  Do NOT do count--. That amounts to trying to mutate 'count' in place. This is the road to perdition.  *********(DONE)
+  This click handler needs to use 'setCount' to set the 'count' to be the
+current 'count' minus one. Do NOT do count--. That amounts to trying to mutate
+'count' in place. This is the road to perdition.  *********(DONE)
 
 STEP 6:
-  This click handler needs to use 'setCount' to set the 'count' to be zero again.  *********(DONE)
+  This click handler needs to use 'setCount' to set the 'count' to be zero
+again.  *********(DONE)
 */
 
-
-import React, { useState } from 'react'; 
-/* STEP 0 */ 
+import React, {useState} from 'react';
+/* STEP 0 */
 export default function Counter() {
-/* STEP 1 */
+  /* STEP 1 */
   const [count, setCount] = useState(0);
   const increment = () => {
-/* STEP 4 */
-  setCount ( count + 1 );
+    /* STEP 4 */
+    setCount(count + 1);
   };
   const decrement = () => {
-/* STEP 5 */
-  setCount ( count - 1 );
+    /* STEP 5 */
+    setCount(count - 1);
   };
   const reset = () => {
-/* STEP 6 */
-  setCount (0);
+    /* STEP 6 */
+    setCount(0);
   };
 
   const style = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: count % 2 === 0 ? 'royalblue': 'crimson',  /* STEP 2 */
+    fontSize : '1.5em',
+    marginBottom : '0.3em',
+    color : count % 2 === 0 ? 'royalblue' : 'crimson', /* STEP 2 */
   };
 
   return (
